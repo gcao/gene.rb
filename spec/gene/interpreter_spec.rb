a58e85ef12f8e5c1bd9905550d646904abf0042d
Guoliang Cao <gcao99@gmail.com>
2013-05-03 17:44:00 -0400

    Spec changes

--- spec/gene/interpreter_spec.rb
+++ spec/gene/interpreter_spec.rb
@@ -2,20 +2,32 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Gene::Interpreter do
   {
-    '1'      => 1,
-    '"a"'    => "a",
-    'null'   => nil,
-    '[]'     => [],
-    '[1]'    => [1],
-    '{}'     => {},
-    '{1 2}'  => {1 => 2},
-    '()'     => Gene::NOOP,
-    '[()]'   => [],
-    '(1)'    => 1,
-    '(1, 2)' => 2,
+    '1'          => 1,
+    '"a"'        => "a",
+    'null'       => nil,
+    '[]'         => [],
+    '[1]'        => [1],
+    '{}'         => {},
+    '{1 2}'      => {1 => 2},
+    '()'         => Gene::NOOP,
+    '[()]'       => [],
+    '(1)'        => 1,
+    '(1 2)'      => 2,
+    '(1 ())'     => Gene::NOOP,
     #'(($$ let a 1) ($$ + a 1))' => 2,
   }.each do |input, result|
     it "process #{input} should work" do
+      parsed = Gene::Parser.new(input).parse
+      Gene::Interpreter.new.run(parsed).should == result
+    end
+  end
+
+  # Copy individual tests to below and run to make debug easier
+  # in vim command line, enter :rspec %:LINE_NUMBER
+  {
+    '(1 2)'      => 2,
+  }.each do |input, result|
+    it "TEMP TEST should work" do
       parsed = Gene::Parser.new(input).parse
       Gene::Interpreter.new.run(parsed).should == result
     end
