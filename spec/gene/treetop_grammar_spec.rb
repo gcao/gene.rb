d0c22918feb00bc4c5200342e0e7908e71fb8db0
Guoliang Cao <gcao99@gmail.com>
2013-10-16 17:30:48 -0400

    Treetop based parser

--- spec/gene/treetop_grammar_spec.rb
+++ spec/gene/treetop_grammar_spec.rb
@@ -6,18 +6,20 @@ describe 'gene/grammar.tt' do
   # Copy individual tests to below and run to make debug easier
   # in vim command line, enter :rspec %:11
   {
-    '1'    => 1,
+    '""'    => '',
   }.each do |input, result|
     it "TEMP TEST should work" do
       parser = Gene::GrammarParser.new
       nodes = parser.parse(input)
       $logger.level = Logem::DEBUG
       nodes.resolve.should == result
-      $logger.level = Logem::INFO
     end
   end
 
   {
+    #'1'    => 1,
+    #'-1.1'    => -1.1,
+    #'""'    => '',
     #'a'    => Gene::Entity.new('a'),
     #'true'    => true,
     #'null'    => nil,
