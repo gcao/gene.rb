83f9c1a36d3efab24adf9c1284e15dfd7466343f
Guoliang Cao <gcao99@gmail.com>
2017-01-08 10:49:49 -0500

    Experimenting with faking modules with upper case method names

--- spec/test.rb
+++ spec/test.rb
@@ -1,10 +1,51 @@
 #!/usr/bin/env ruby
 
-source = %q{
-  args           = {}
-  args['q']      = 'test'
-  args.to_a.collect {|x| "#{x[0]}=#{x[1]}" }.join('&')
-}
+module FakeModule
+  def self.included target
+    puts 'FakeModule module'
+    # Logic goes here
+  end
+end
 
-puts RubyVM::InstructionSequence.new(source).eval
+def FakeModule(*args)
+  Module.new do
+    define_singleton_method :included do |target|
+      puts "FakeModule(#{args.inspect[1..-2]})"
+      # Logic goes here
+    end
+  end
+end
+
+module Lib
+  module FakeModule2
+    def self.included target
+      puts 'FakeModule2 module'
+      # Logic goes here
+    end
+  end
+
+  def self.FakeModule2(*args)
+    Module.new do
+      define_singleton_method :included do |target|
+        puts "FakeModule2(#{args.inspect[1..-2]})"
+        # Logic goes here
+      end
+    end
+  end
+end
+
+class A
+  include FakeModule
+  include FakeModule('test')
+  include Lib::FakeModule2
+  include Lib::FakeModule2('test')
+end
+
+#source = %q{
+#  args           = {}
+#  args['q']      = 'test'
+#  args.to_a.collect {|x| "#{x[0]}=#{x[1]}" }.join('&')
+#}
+
+#puts RubyVM::InstructionSequence.new(source).eval
 
