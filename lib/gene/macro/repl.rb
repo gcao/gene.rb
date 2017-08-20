29b427221c28dba42f0a10a6446fc3079eba052b
Guoliang Cao <gcao99@gmail.com>
2017-08-20 13:36:35 -0400

    Macro repl

--- lib/gene/macro/repl.rb
+++ lib/gene/macro/repl.rb
@@ -1,30 +1,37 @@
-class Gene::Macro::Repl
-  attr_reader :interpreter
-  attr_accessor :_
+require "readline"
 
+class Gene::Macro::Repl
   def initialize
     @interpreter = Gene::Macro::Interpreter.new
+    # Define _ on the interpreter to save last result
+    @interpreter.class.send :attr_accessor, :_
   end
 
   def start
-    loop do
+    while input = Readline.readline("GM> ", true)
       begin
-        print "MACRO> "
-
-        input = gets.chomp
         if input =~ /^_/
-          p instance_eval(input)
+          @interpreter._ = @interpreter.instance_eval(input)
+        elsif input.strip == 'exit'
+          puts "Exiting..."
+          break
+        elsif input.strip.empty?
+          next
+        elsif input =~ /^(self)?\.(.*)$/
+          @interpreter._ = @interpreter.instance_eval("self.#{$2}")
         else
-          self._ = @interpreter.parse_and_process input
-          puts self._
+          @interpreter._ = @interpreter.parse_and_process input
         end
+
+        p @interpreter._
+        puts
       rescue
         puts "#{$!.class}: #{$!}"
         puts $!.backtrace.map{|line| "\t#{line}" }.join("\n")
+        puts
       rescue RuntimeError
         puts "#{$!.class}: #{$!}"
         puts $!.backtrace.map{|line| "\t#{line}" }.join("\n")
-      ensure
         puts
       end
     end
