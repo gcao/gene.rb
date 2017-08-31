55c90ac917e61bfe3e3e043878baae3b8574ef49
Guoliang Cao <gcao99@gmail.com>
2017-08-31 08:51:05 -0400

    Minor changes

--- spec/gene/gene_lang_serialization_spec.rb
+++ spec/gene/gene_lang_serialization_spec.rb
@@ -5,14 +5,23 @@ describe Gene::Lang do
     @interpreter = Gene::Lang::Interpreter.new
   end
 
-  it "serialization" do
-    result = Gene::Lang.serialize @interpreter.scope
-    result.should == '{"#class": "Gene::Lang::Scope", "parent": null, "variables": {}, "arguments": []}'
+  describe "serialization" do
+    it "_scope" do
+      result = Gene::Lang.serialize @interpreter.parse_and_process(example.description)
+      result.should == '{"#class": "Gene::Lang::Scope", "parent": null, "variables": {}, "arguments": []}'
+    end
+
+    it "(class A)" do
+      result = Gene::Lang.serialize @interpreter.parse_and_process(example.description)
+      result.should == '{"#class": "Gene::Lang::Class", "name": "A", "instance_methods": {}, "properties": {}}'
+    end
   end
 
-  it "deserialization" do
-    serialized = Gene::Lang.serialize @interpreter.scope
-    result = Gene::Lang.deserialize serialized
-    result.class.should == Gene::Lang::Scope
+  describe "deserialization" do
+    it "_scope" do
+      serialized = Gene::Lang.serialize @interpreter.parse_and_process(example.description)
+      result = Gene::Lang.deserialize serialized
+      result.class.should == Gene::Lang::Scope
+    end
   end
 end
