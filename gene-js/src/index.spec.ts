c50f730447bb1b0093d78d198c219bcfa00a7812
Guoliang Cao <gcao99@gmail.com>
2018-02-04 08:52:38 -0500

    Compile Gene to js

--- gene-js/src/index.spec.ts
+++ gene-js/src/index.spec.ts
@@ -1,9 +1,9 @@
-describe('Gene', () => {
-  // Read more about fake timers: http://facebook.github.io/jest/docs/en/timer-mocks.html#content
-  jest.useFakeTimers();
+// describe('Gene', () => {
+//   // Read more about fake timers: http://facebook.github.io/jest/docs/en/timer-mocks.html#content
+//   jest.useFakeTimers();
 
-  // Assert if setTimeout was called properly
-  it('should work', () => {
-  });
+//   // Assert if setTimeout was called properly
+//   it('should work', () => {
+//   });
 
-});
+// });
