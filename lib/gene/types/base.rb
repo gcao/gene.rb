48301895a195cb11590111c5abf7c39aecf092d6
Guoliang Cao <gcao99@gmail.com>
2018-05-31 12:08:42 -0400

    Base#to_s

--- lib/gene/types/base.rb
+++ lib/gene/types/base.rb
@@ -39,10 +39,71 @@ class Gene::Types::Base
   def to_s
     s = "("
     s << type.to_s
+
+    @properties.each do |name, value|
+      next if name.to_s =~ /^\$/
+      next if %W(#type #class #data).include? name.to_s
+
+      s << " "
+
+      if value == true
+        s << "^^#{name}"
+      elsif value == false
+        s << "^!#{name}"
+      elsif value.is_a? Gene::Types::Base
+        s << "^#{name}" << value.to_s_short
+      elsif value.class.name == "Array"
+        s << "^#{name} "
+        if value.empty?
+          s << "[]"
+        else
+          s << "[...]"
+        end
+      elsif value.class.name == "Hash"
+        s << "^#{name} "
+        if value.empty?
+          s << "{}"
+        else
+          s << "{...}"
+        end
+      else
+        s << "^#{name} " << value.inspect
+      end
+    end
+
     data.each do |child|
-      s << ' ' << child.inspect
+      s << ' '
+
+      if child.is_a? Gene::Types::Base
+        s << child.to_s_short
+      elsif child.class.name == "Array"
+        if child.empty?
+          s << "[]"
+        else
+          s << "[...]"
+        end
+      elsif child.class.name == "Hash"
+        if child.empty?
+          s << "{}"
+        else
+          s << "{...}"
+        end
+      else
+        s << child.inspect
+      end
     end
     s << ")"
   end
   alias inspect to_s
+
+  def to_s_short
+    s = "("
+    s << type.to_s
+
+    if not @data.empty? or not @properties.empty?
+      s << " ..."
+    end
+
+    s << ")"
+  end
 end
